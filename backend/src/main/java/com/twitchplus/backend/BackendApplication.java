9fdfca2c1ebe9a296eb3006caa838a777782bb33
226wyj <308033240@qq.com>
2024-09-05 17:01:34 -0700

    first commit

---
+++ backend/src/main/java/com/twitchplus/backend/BackendApplication.java
@@ -0,0 +1,15 @@
+package com.twitchplus.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication
+@EnableFeignClients
+public class BackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendApplication.class, args);
+    }
+
+}
