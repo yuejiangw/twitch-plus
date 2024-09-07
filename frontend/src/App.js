32e14833bc6188ae30dea66e47d2f7fa93ced5cf
226wyj <308033240@qq.com>
2024-09-06 20:46:49 -0700

    fix antd version, setup basic layout and CSS

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,25 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Layout } from 'antd';
+
+const { Header, Content, Sider } = Layout;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+      <Layout>
+        <Header>
+          {'Header'}
+        </Header>
+        <Layout>
+          <Sider width={300} className="site-layout-background">
+            {'Sider'}
+          </Sider>
+          <Layout style={{ padding: '24px' }}>
+            <Content
+                className="site-layout-background"
+                style={{
+                  padding: 24,
+                  margin: 0,
+                  height: 800,
+                  overflow: 'auto'
+                }}
+            >
+              {'Home'}
+            </Content>
+          </Layout>
+        </Layout>
+      </Layout>
+  )
 }
 
 export default App;
