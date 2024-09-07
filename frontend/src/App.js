1bb5edcb75a306d9bcd522f51731752534c2516e
226wyj <308033240@qq.com>
2024-09-06 20:56:53 -0700

    beautify and add top games by default

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
-import React, { useState } from 'react';
-import { Layout, message } from 'antd';
-import { logout, getFavoriteItem } from './utils';
+import React, { useState, useEffect } from 'react';
+import { Layout, message, Menu } from 'antd';
+import { LikeOutlined, FireOutlined } from '@ant-design/icons';
+import { logout, getFavoriteItem, getTopGames } from './utils';
 import PageHeader from './components/PageHeader';
 import CustomSearch from './components/CustomSearch';
 
@@ -9,6 +10,16 @@ const { Header, Content, Sider } = Layout;
 function App() {
     const [loggedIn, setLoggedIn] = useState(false)
     const [favoriteItems, setFavoriteItems] = useState([]);
+    const [topGames, setTopGames] = useState([])
+
+    useEffect(() => {
+        getTopGames()
+            .then((data) => {
+                setTopGames(data)
+            }).catch((err) => {
+            message.error(err.message)
+        })
+    }, [])
 
     const signinOnSuccess = () => {
         setLoggedIn(true);
@@ -26,6 +37,30 @@ function App() {
         })
     }
 
+    const mapTopGamesToProps = (topGames) => [
+        {
+            label: "Recommend for you!",
+            key: "recommendation",
+            icon: <LikeOutlined />,
+        },
+        {
+            label: "Popular Games",
+            key: "popular_games",
+            icon: <FireOutlined />,
+            children: topGames.map((game) => ({
+                label: game.name,
+                key: game.id,
+                icon:
+                    <img
+                        alt="placeholder"
+                        src={game.box_art_url.replace('{height}', '40').replace('{width}', '40')}
+                        style={{ borderRadius: '50%', marginRight: '20px' }}
+                    />
+            }))
+        }
+    ]
+
+
     return (
         <Layout>
             <Header>
@@ -39,6 +74,12 @@ function App() {
             <Layout>
                 <Sider width={300} className="site-layout-background">
                     <CustomSearch onSuccess={() => { }} />
+                    <Menu
+                        mode="inline"
+                        onSelect={() => { }}
+                        style={{ marginTop: '10px' }}
+                        items={mapTopGamesToProps(topGames)}
+                    />
                 </Sider>
                 <Layout style={{ padding: '24px' }}>
                     <Content
